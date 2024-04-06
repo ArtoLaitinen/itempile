f5af0d2399f4fde61b59a3c60dd2f8fefae0ca61
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-06 17:23:16 +0300

    added mysql connection pool
    added mysql2 as a dependency.
    added mysql connection pool

---
+++ server/db/pool.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 10,
+  waitForConnections: true,
+});
+
+module.exports = pool;
