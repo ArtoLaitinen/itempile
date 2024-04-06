2da5adeaf0454832fb4703b592fa3a4268d214a8
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-06 18:09:17 +0300

    Revert "disabled a lint rule because it didnt work properly"
    This reverts commit 499df191926e1732de985fc2cd9231eac6c702e3.

--- server/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { signUpUser } = require('../controllers/users.js');
-
-router.post('/signup', signUpUser);
-
-module.exports = router;
