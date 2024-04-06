6bbe222da05cbf0a00cfa61c2a45e714b44b0286
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-06 16:35:19 +0300

    changed the server port to 5000

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require('./app.js');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.info(`BACKEND IS LISTENING ON PORT ${PORT}`);
