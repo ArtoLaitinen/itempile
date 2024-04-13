70e7998247aedca13be7f6e9fad6132a026d3814
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 15:11:13 +0300

    added findItemById method to items model

--- server/models/items.js
+++ server/models/items.js
@@ -15,6 +15,22 @@ const items = {
       throw new Error(error);
     }
   },
+
+  findItemById: async (id) => {
+    try {
+      const connection = await pool.getConnection();
+      const selectQuery = `SELECT items.*, users.id as user_id, users.name as user_name, users.email as user_email
+      FROM items
+      INNER JOIN users ON items.owner_id = users.id
+      WHERE items.id=?`;
+
+      const [results] = await connection.query(selectQuery, [id]);
+      connection.release();
+      return results;
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
 };
 
 module.exports = items;
