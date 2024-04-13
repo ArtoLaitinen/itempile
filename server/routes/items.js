d3e0cc5816f4bdd35d47da78f7c296a78b826a95
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 17:20:11 +0300

    made the getItemsByUserId, post, put and delete items routes use the middleware

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,15 +2,16 @@ const express = require('express');
 const {
   getItems, getItemById, getItemsByUserId, createItem, updateItem, deleteItem,
 } = require('../controllers/items.js');
+const verifyToken = require('../middleware/verifyToken.js');
 
 const router = express.Router();
 
 router.get('/', getItems);
 router.get('/:id', getItemById);
 
-// this should also require token
-router.get('/user/:userId', getItemsByUserId);
+router.use(verifyToken);
 
+router.get('/user/:userId', getItemsByUserId);
 router.post('/', createItem);
 router.put('/:id', updateItem);
 router.delete('/:id', deleteItem);
