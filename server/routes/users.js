90734e1802ac4cd85cbaf6c50c2173310db78e42
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 11:51:55 +0300

    added the login endpoint to the user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { signUpUser } = require('../controllers/users.js');
+const { signUpUser, loginUser } = require('../controllers/users.js');
 
 router.post('/signup', signUpUser);
+router.post('/login', loginUser);
 
 module.exports = router;
