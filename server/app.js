4d13ce19408bed98e59a928a5e2783b57ef4fc1c
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 14:43:49 +0300

    added an endpoint for items requests

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const usersRouter = require('./routes/users.js');
+const itemsRouter = require('./routes/items.js');
 
 const app = express();
 app.use(express.json());
@@ -10,5 +11,6 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/api/users', usersRouter);
+app.use('/api/items', itemsRouter);
 
 module.exports = app;
