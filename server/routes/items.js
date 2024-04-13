ae1f66286648accce7d65cad508f33148871675e
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 15:41:53 +0300

    added endpoint for getting items by userId to items routes

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { getItems, getItemById } = require('../controllers/items.js');
+const { getItems, getItemById, getItemsByUserId } = require('../controllers/items.js');
 
 const router = express.Router();
 
 router.get('/', getItems);
 router.get('/:id', getItemById);
+
+// this should also require token
+router.get('/user/:userId', getItemsByUserId);
 
 module.exports = router;
