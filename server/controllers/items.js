0a1351845dbfb2b424abc99b08e70564b26ee131
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 15:12:00 +0300

    added getItemById function to items controllers
    also fixed getItems method to return

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -4,13 +4,32 @@ const getItems = async (req, res) => {
   try {
     const response = await items.findItems();
     if (response) {
-      res.json(response);
+      return res.json(response);
     }
+
+    return res.status(500).json({ message: 'Something went wrong' });
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+    return res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
+const getItemById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+
+    const response = await items.findItemById(id);
+
+    if (response.length === 0) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    return res.json(response[0]);
+  } catch (error) {
+    return res.status(500).json({ message: 'Something went wrong' });
   }
 };
 
 module.exports = {
   getItems,
+  getItemById,
 };
