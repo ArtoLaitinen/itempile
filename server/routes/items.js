1116ad97aee709616b4735c0d42a14a44bbd0da0
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 15:14:59 +0300

    add the the getItemById endpoint to the items routes

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getItems } = require('../controllers/items.js');
+const { getItems, getItemById } = require('../controllers/items.js');
 
 const router = express.Router();
 
 router.get('/', getItems);
+router.get('/:id', getItemById);
 
 module.exports = router;
