9ffb6398599c6510bed5d37237b9f44abcae6663
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-13 12:49:30 +0300

    added tests for the login endpoint

--- server/__tests__/users.test.js
+++ server/__tests__/users.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcryptjs');
+const { beforeEach } = require('@jest/globals');
 const app = require('../app.js');
 const users = require('../models/users.js');
 
@@ -104,6 +105,98 @@ describe('Users', () => {
 
       expect(response.status).toEqual(400);
       expect(response.body).toEqual({ message: '"email" must be a valid email' });
+    });
+  });
+
+  describe('login endpoint', () => {
+    beforeEach(() => {
+      bcrypt.compare.mockResolvedValue(true);
+      users.findByEmail.mockResolvedValue([{ id: uuidv4(), email: 'test-user@email.com', password_hash: 'hashedPassword' }]);
+    });
+
+    test('should login a created user', async () => {
+      const userCredentials = {
+        email: 'test-user@email.com',
+        password: 'test_password',
+      };
+
+      const response = await request(app)
+        .post('/api/users/login')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(userCredentials);
+
+      expect(response.status).toEqual(201);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body.id).toBeTruthy();
+      expect(response.body.email).toEqual('test-user@email.com');
+      expect(response.body.token).toBeTruthy();
+    });
+
+    test('should fail with wrong password', async () => {
+      bcrypt.compare.mockResolvedValue(false);
+
+      const userCredentials = {
+        email: 'test-user@email.com',
+        password: 'wrong_password',
+      };
+
+      const response = await request(app)
+        .post('/api/users/login')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(userCredentials);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual({ message: 'Could not identify user, credentials might be wrong' });
+    });
+
+    test('should fail if the user doesnt exist', async () => {
+      users.findByEmail.mockResolvedValue([]);
+      const userCredentials = {
+        email: 'NaN@email.com',
+        password: 'test_password',
+      };
+
+      const response = await request(app)
+        .post('/api/users/login')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(userCredentials);
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual({ message: 'Could not identify user, credentials might be wrong' });
+    });
+
+    test('should fail if one of the parameters is missing', async () => {
+      const userCredentials = {
+        email: 'test-user@email.com',
+      };
+
+      const response = await request(app)
+        .post('/api/users/login')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(userCredentials);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual({ message: '"password" is required' });
+    });
+
+    test('should fail if one of the parameters is empty', async () => {
+      const userCredentials = {
+        email: '',
+        password: 'test_password',
+      };
+
+      const response = await request(app)
+        .post('/api/users/login')
+        .set('Accept', 'application/json')
+        .set('Content-Type', 'application/json')
+        .send(userCredentials);
+
+      expect(response.status).toEqual(400);
+      expect(response.body).toEqual({ message: '"email" is not allowed to be empty' });
     });
   });
 });
