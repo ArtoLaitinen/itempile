eed716f48670d95207844c9348068b9fa7054fda
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-20 19:17:01 +0300

    added item component

---
+++ client/src/components/Item.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Item.css";
+
+function Item({ item }) {
+  return (
+    <>
+      <h2 className="item-title-text">{item.title}</h2>
+
+      <h2 className="item-price-text">{item.price}</h2>
+
+      <div className="image-container">
+        <img src={item.image} alt={item.image} />
+      </div>
+    </>
+  );
+}
+
+export default Item;
