a3c11775308dadb801f69fe38fb86543076bc6ed
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-20 16:12:03 +0300

    changed getItems to await the response.json

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 export const getItems = async () => {
   const response = await fetch(`${import.meta.env.VITE_API_URL}/api/items`);
-  return response.json();
+  const responseJson = await response.json();
+  return responseJson;
 };
