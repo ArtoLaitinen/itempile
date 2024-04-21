13d511f6cec25714c3e5b091aef680cab556ecef
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-21 13:11:13 +0300

    added getItemById function to items.js in api folder

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -1,6 +1,14 @@
-/* eslint-disable import/prefer-default-export */
 export const getItems = async () => {
   const response = await fetch(`${import.meta.env.VITE_API_URL}/api/items`);
+  const responseJson = await response.json();
+
+  return responseJson;
+};
+
+export const getItemById = async (itemId) => {
+  const response = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/items/${itemId}`,
+  );
   const responseJson = await response.json();
 
   return responseJson;
