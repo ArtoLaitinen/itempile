a8a7471a001169f4a836a1fce24d7979cc5a3238
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-22 12:07:26 +0300

    added the usage of AuthContex to app.jsx
    app.jsx now has AuthContext Provider that gives the values for the context. the login function is used to login the user with either new expiry or old one depending on if the user logs in by themself or just refreshes the page. the useEffect with login as dep is used to keep people logged in even if they refresh the page. the expiration of the session is handled in the other useEffect that logs the user out if session has expired

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import RootLayout from "./pages/RootLayout";
 import AllItemsPage from "./pages/AllItemsPage";
 import ItemPage from "./pages/ItemPage";
+import AuthContext from "./utils/AuthContext";
 
 import "./App.css";
 
@@ -15,7 +16,67 @@ const queryClient = new QueryClient({
   },
 });
 
+let logoutTimer;
+
 function App() {
+  // states used for authorization
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState(null);
+
+  const login = useCallback((uid, authToken, expirationDate) => {
+    setUserId(uid);
+    setToken(authToken);
+    // if no expiry yet: current date + 1h
+    const newTokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+
+    setTokenExpirationDate(newTokenExpirationDate);
+
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        uid,
+        authToken,
+        expiration: newTokenExpirationDate.toISOString(),
+      }),
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setUserId(null);
+    setToken(null);
+    setTokenExpirationDate(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  // check if user has valid token in localStorage and use it to login
+  useEffect(() => {
+    const storedUserData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedUserData &&
+      storedUserData.token &&
+      new Date(storedUserData.expiration) > new Date()
+    ) {
+      login(
+        storedUserData.uid,
+        storedUserData.token,
+        new Date(storedUserData.expiration),
+      );
+    }
+  }, [login]);
+
+  // logout the user when expirationDate has passed
+  useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -27,10 +88,23 @@ function App() {
     },
   ]);
 
+  const authContextValue = useMemo(
+    () => ({
+      isLoggedIn: !!token,
+      token,
+      userId,
+      login,
+      logout,
+    }),
+    [token, userId, login, logout],
+  );
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthContext.Provider value={authContextValue}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthContext.Provider>
   );
 }
 
