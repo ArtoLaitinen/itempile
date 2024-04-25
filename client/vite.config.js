4aba2c4d93c713149a3acffb5f110a20fc3228fa
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-25 16:31:43 +0300

    added test to vite config and added test setup file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
+export default () => {
+  return defineConfig({
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.js",
+    },
+    esbuild: {
+      loader: "jsx",
+      include: /(src)\/.*\.jsx?$/,
+      exclude: [],
+    },
+  });
+};
