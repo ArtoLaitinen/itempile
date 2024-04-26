5f1c54796d791be14ac8bbc118060da2d1f50f37
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-26 16:14:36 +0300

    added categories file to utils

---
+++ client/src/utils/Categories.js
@@ -0,0 +1,20 @@
+const Categories = [
+  "Furniture",
+  "Home Appliances",
+  "Garden & Renovation",
+  "Electronics",
+  "Office",
+  "Vehicles",
+  "Clothes",
+  "Jewelry",
+  "Pets",
+  "Sports",
+  "Toys & Games",
+  "Books",
+  "Music",
+  "Movies",
+  "Arts",
+  "Other",
+];
+
+export default Categories;
