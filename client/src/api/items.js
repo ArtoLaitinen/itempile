9dddedf993bfce1bb4e13dfd49a9ac7a256ebc9a
Arto-Laitinen <arto.e.laitinen@tuni.fi>
2024-04-27 11:31:35 +0300

    added getItemsByUserId to items api

--- client/src/api/items.js
+++ client/src/api/items.js
@@ -43,6 +43,34 @@ export const getItemById = async (itemId) => {
   }
 };
 
+export const getItemsByUserId = async (userId, token) => {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/items/user/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+      if (responseJson.message) {
+        throw new Error(responseJson.message);
+      }
+      throw new Error("An error has occurred, please try again");
+    }
+
+    return responseJson;
+  } catch (error) {
+    if (error instanceof TypeError) {
+      throw new Error("Server is unreachable. Please try again later.");
+    }
+    throw error;
+  }
+};
+
 export const addItem = async ({
   title,
   description,
